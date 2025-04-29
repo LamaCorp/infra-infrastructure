3d56ba3d77f3afb8f615614a7f562c96322f731e
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2025-04-29 17:45:15 +0200

    k8s: observability: configure promtail
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- vault/k8s-global.tf
+++ vault/k8s-global.tf
@@ -14,11 +14,3 @@ resource "vault_generic_secret" "k8s-global_core-observability_alertmanager_disc
     infra-critical-alerts = "FIXME: this should be a slack webhook pointing to the #infra-critical-alerts channel."
   })
 }
-
-resource "vault_generic_secret" "k8s-global_core-observability_loki-auth" {
-  path = "${vault_mount.k8s-global.path}/core-observability/loki-auth"
-  data_json = jsonencode({
-    username = "infra"
-    password = random_password.k8s-k3s-fsn-as212024-net_infra-observability_loki-auth-map["infra"].result
-  })
-}
