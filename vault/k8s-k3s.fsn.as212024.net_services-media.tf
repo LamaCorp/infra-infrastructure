42a98b016cb378dc59a352a37bb5afe8f92f4a00
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2025-08-30 23:14:06 +0200

    k8s/services/media: try fixing qbittorrent vpn
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

--- vault/k8s-k3s.fsn.as212024.net_services-media.tf
+++ vault/k8s-k3s.fsn.as212024.net_services-media.tf
@@ -2,6 +2,7 @@ resource "vault_generic_secret" "k8s-k3s-fsn-as212024-net_services-media_qbittor
   path         = "${vault_mount.k8s-clusters["k3s.fsn.as212024.net"].path}/services-media/qbittorrent/vpn"
   disable_read = true
   data_json = jsonencode({
-    "wg0.conf" = "FIXME: get this from Proton VPN"
+    username = "FIXME: get this from Proton VPN"
+    password = "FIXME: get this from Proton VPN"
   })
 }
